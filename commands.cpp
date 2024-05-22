8c06d92512d148964b6e74e82f60b6a94c127b3d
Akaash Suresh <97249336+rzredg@users.noreply.github.com>
2024-05-21 17:26:15 -0700

    Add files via upload

---
+++ commands.cpp
@@ -0,0 +1,13 @@
+#include "commands.h"
+
+bool Commands::readMoviesFile() { 
+	return false; 
+}
+
+bool Commands::readCustomersFile() { 
+	return false; 
+}
+
+bool Commands::readCommandsFile() { 
+	return false; 
+}
