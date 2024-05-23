9ed7e98b8dffa08d5bc8a67129eac36042b6cdc0
akaas <akaashsuresh360@gmail.com>
2024-05-22 20:00:32 -0700

    customer class

--- customer.cpp
+++ customer.cpp
@@ -1,1 +1,52 @@
 #include "customer.h"
+
+Customer::Customer(int id, string ln, string fn) {
+    this->customerID = id;
+    this->lastName = ln;
+    this->firstName = fn;
+}
+
+int Customer::getCustomerID() { 
+    return customerID;
+}
+
+string Customer::getLastName() { 
+    return lastName;
+}
+
+string Customer::getFirstName() { 
+    return firstName; 
+}
+
+bool Customer::hasMovie(string title) {
+    for (int i = 0; i <= borrowedMovies.size(); i++) {
+        if (borrowedMovies[i] == title) {
+            return true;
+        }
+    } 
+    return false; 
+}
+
+bool Customer::borrowMovie(string title) {
+    for (int i = 0; i <= borrowedMovies.size(); i++) {
+        if (borrowedMovies[i] == title) {
+            return false;
+        }
+    }
+    borrowedMovies.push_back(title);
+    return true;
+}
+
+bool Customer::returnMovie(string title) {
+    for (int i = 0; i <= borrowedMovies.size(); i++) {
+        if (borrowedMovies[i] == title) {
+            borrowedMovies.erase(borrowedMovies.begin() + i - 1);
+            return true;
+        }
+    } 
+    return false; 
+}
+
+string Customer::toCustomerString() const { 
+    return to_string(customerID) + " " + lastName + " " + firstName;
+}
--- customer.h
+++ customer.h
@@ -2,6 +2,7 @@
 #define CUSTOMER_H
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Customer {
@@ -10,11 +11,16 @@ private:
   int customerID;
   string lastName;
   string firstName;
+  vector<string> borrowedMovies;
 
 public:
   Customer(int id, std::string ln, std::string fn);
   int getCustomerID();
   string getLastName();
   string getFirstName();
+  bool hasMovie(string title);
+  bool borrowMovie(string title);
+  bool returnMovie(string title);
+  string toCustomerString() const;
 };
 #endif
