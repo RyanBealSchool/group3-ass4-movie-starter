4f3f37cf17c62a46887b8884254cd21a9402ae99
RyanBealSchool <rjbeal1@uw.edu>
2024-05-24 15:32:10 -0700

    First implementation of the Media class

--- media.cpp
+++ media.cpp
@@ -1,1 +1,34 @@
 #include "media.h"
+
+Media::Media(string mediaTitle, int mediaStock) {
+	this->title = mediaTitle;
+	this->stock = mediaStock;
+}
+
+bool Media::borrowMedia()
+{
+	if (this->stock > 0)
+	{
+		this->stock--;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void Media::returnMedia()
+{
+	this->stock++;
+}
+
+void Media::readObjFromStream(istream& stream)
+{
+}
+
+istream& operator>>(istream& stream, Media& inMedia)
+{
+	inMedia.readObjFromStream(stream);
+	return stream;
+}
--- media.h
+++ media.h
@@ -3,7 +3,21 @@
 #include <iostream>
 using namespace std;
 
-class Media{
-    
+class Media {
+public:
+	Media(string mediaTitle, int mediaStock); //constructor for media class
+	bool borrowMedia(); //borrow the media (true:stock was taken | false:there was no more stock)
+	void returnMedia(); //return the media
+	virtual string toMediaString() = 0; //get the media as a string
+
+	virtual bool operator<(Media compMedia) = 0; //compare if this media is less than the passed in
+	virtual bool operator==(Media compMedia) = 0; //compare if this media is greater than the passed in
+	friend istream& operator>>(istream& stream, Media &inMedia); //read in a media from the stream
+
+private:
+	virtual void readObjFromStream(istream& stream); //virtual function sued in operator>> to make reading in objects virtual
+
+	string title; // the name of the media
+	int stock; // the current stock of the media
 };
 #endif
