9cf35a64910b2c3dba033a282c2965cf537dfcf2
akaas <akaashsuresh360@gmail.com>
2024-05-24 17:37:24 -0700

    pq

--- inventory.h
+++ inventory.h
@@ -2,17 +2,18 @@
 #define INVENTORY_H
 #include "hash_table.h"
 #include "movie.h"
+#include "media.h"
 #include <iostream>
 #include <queue>
 #include <string>
 
 class Inventory {
 private:
-  priority_queue<Movie> sortedMovies;
+  //priority_queue<Media> sortedMovies;
   HashTable hashTable;
 
 public:
-  void addMovie(Movie *m);
-  Movie *findMovie(std::string title);
+  void addMovie(Media *m);
+  Media *findMovie(string title);
 };
 #endif
--- movie.h
+++ movie.h
@@ -10,6 +10,7 @@ class Movie : public Media{
 public:
 	explicit Movie(); //default constructor
 	virtual string toMediaString() const; //format the movie string shared by all children (year is different for classics)
+	bool operator<(const Movie& other) const; // < operator to compare movies with a priority queue
 
 protected:
 	void initalizeMovie(const int& movieStock, const string movieDirector, const string& movieTitle, const int movieYear); //used for constuctor var assignment of children classes
