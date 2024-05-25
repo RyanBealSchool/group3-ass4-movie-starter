2d66284b07946b4867f0ba58c07a782924d6a5b9
akaas <akaashsuresh360@gmail.com>
2024-05-24 17:24:54 -0700

    hashtable class

--- hash_table.cpp
+++ hash_table.cpp
@@ -1,1 +1,25 @@
 #include "hash_table.h"
+
+HashTable::HashTable() {
+    tableSize = 0;
+}
+
+bool HashTable::add(const string &key, Media *media) { 
+    table[key] = {key, media};
+}
+
+Media *HashTable::findMovie(string key) { 
+    auto it = table.find(key);
+    if (it != table.end()) {
+        return it->second.media;
+    }
+    return nullptr;
+}
+
+int HashTable::hashFunction(int key) { 
+    return key % static_cast<int>(table.bucket_count()); 
+}
+
+int HashTable::getTableSize() { 
+    return table.size();
+}
--- hash_table.h
+++ hash_table.h
@@ -1,14 +1,30 @@
-#include "movie.h"
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
 #include <map>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <functional>
+#include "media.h"
+#include "movie.h"
+using namespace std;
 
 class HashTable {
 private:
-  map<int, Movie *> table;
   int tableSize;
-  int hashFunction(int key);
+  struct HashEntry {
+        string key;
+        Media* media;
+    };
+    unordered_map<string, HashEntry> table;
+    //map<int, Movie *> table;
 
 public:
   HashTable();
-  void addMovie(Movie *movie);
-  Movie *findMovie(string key);
+  //void addMovie(Movie *movie);
+  bool add(const string& key, Media* media);
+  Media *findMovie(string key);
+  int hashFunction(int key);
+  int getTableSize();
 };
+#endif
