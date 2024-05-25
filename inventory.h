4077beff35cd5d941b3bab8e09dab43add184f64
akaas <akaashsuresh360@gmail.com>
2024-05-24 20:30:57 -0700

    inventory1

--- inventory.h
+++ inventory.h
@@ -6,14 +6,22 @@
 #include <iostream>
 #include <queue>
 #include <string>
+#include <vector>
+#include "customer.h"
+using namespace std;
 
 class Inventory {
 private:
   //priority_queue<Media> sortedMovies;
   HashTable hashTable;
+  vector<Customer> customers;
 
 public:
-  void addMovie(Media *m);
-  Media *findMovie(string title);
+  void addMedia(Media *m); //adds the media to the hashtable
+  Media *getMedia(string title); //if doesMediaExist = true, finds & returns media
+  bool doesMediaExist(string title); //checks if the media exists in the hashtable
+  int indexOfMedia(string title); //index of where media is stored in the hashtable
+  void printCustomerList(); //prints out a list of all registered customers
+  string toMediaString(); //prints out a list of all stored media
 };
 #endif
